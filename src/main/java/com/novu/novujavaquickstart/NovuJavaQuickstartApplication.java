19704c6934b2eeec43dba7026b87a451b3947a7f
basfar <mukhtaronif@gmail.com>
2023-08-18 16:16:22 +0100

    Java QuickStart

---
+++ src/main/java/com/novu/novujavaquickstart/NovuJavaQuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.novu.novujavaquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovuJavaQuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovuJavaQuickstartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novu/novujavaquickstart/NovuJavaQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novu.novujavaquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovuJavaQuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
